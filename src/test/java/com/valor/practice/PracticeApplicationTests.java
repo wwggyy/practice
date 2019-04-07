e74f1bbba4dee9c553d8b3fdc7c368361368dacb
valor.wang <wwggyy_2@126.com>
2019-04-07 21:15:42 +0800

    f

--- src/test/java/com/valor/practice/PracticeApplicationTests.java
+++ src/test/java/com/valor/practice/PracticeApplicationTests.java
@@ -1,16 +1,23 @@
 package com.valor.practice;
 
+import com.valor.practice.service.MailService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PracticeApplicationTests {
 
+    @Resource
+    private MailService service;
+
     @Test
-    public void contextLoads() {
+    public void sendSampleMailTest() {
+        service.sendSampleMail("565637570@qq.com","a test"," sth is going!");
     }
 
 }
